45bce3d74b2ce02c1d0b6c434c6935bfe19c655c
Jose Vitor Quadrado <josevitorquadrado@gmail.com>
2022-08-16 15:34:47 -0300

    Add project files.

---
+++ StoreProject/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using StoreProject.Entities;
+using StoreProject.Entities.Enums;
+
+namespace StoreProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.Write("How many items to this order? ");
+            int itemsQuantity = int.Parse(Console.ReadLine());
+            Order order = new Order(DateTime.Now, status, client);
+
+            for (int i = 1; i <= itemsQuantity; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int productQuantity = int.Parse(Console.ReadLine());
+                Product product = new Product(productName, price);
+                OrderItem orderItem = new OrderItem(productQuantity, price, product);
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(order);
+        }
+    }
+}
